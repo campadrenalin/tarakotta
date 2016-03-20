1abb7735f38dccf5388ac8018666d438eca793d9
Philip Horger <campadrenalin@gmail.com>
2016-03-20 15:32:40 -0700

    Add profiler for optimization research

--- main.lua
+++ main.lua
@@ -1,6 +1,9 @@
 local Level = require "level"
+require "util/profiler"
 
 function love.load()
+    profiler = newProfiler()
+    profiler:start()
     Level.switchTo("honeycomb")
 end
 
@@ -13,9 +16,17 @@ function love.update(dt)
 
     -- Handle key events
     if love.keyboard.isDown("escape") then
+        reportProfile()
         love.event.quit()
     end
     if love.keyboard.isDown("r") then
         Level.switchTo(Level.current.name)
     end
+end
+
+function reportProfile()
+    profiler:stop()
+    local outfile = io.open( "profile.txt", "w+" )
+    profiler:report( outfile )
+    outfile:close()
 end
