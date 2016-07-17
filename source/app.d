5e4daa210621287a9d81692e21b8d72d66596927
Philip Horger <campadrenalin@gmail.com>
2016-07-17 00:30:23 -0700

    Switch to dlang

---
+++ source/app.d
@@ -0,0 +1,7 @@
+module tarakotta;
+import sdl_app;
+
+void main() {
+	auto app = new sdl_app.SDLApplication(320,680, "Tarakotta");
+	app.run();
+}
---
+++ source/renderer.d
@@ -0,0 +1,157 @@
+module renderer;
+import std.math;
+import std.conv;
+import std.stdio;
+
+import derelict.sdl2.sdl;
+import derelict.opengl3.gl3;
+
+import glamour.vao: VAO;
+import glamour.shader: Shader;
+import glamour.vbo: Buffer;
+
+struct Color {
+	float r, g, b;
+}
+struct Circle {
+	float x, y, radius;
+	Color color;
+}
+
+Circle[] circles = [
+	{ .80, .20, .1, { 1, 0, 0 } },
+	{ .20, .20, .1, { 1, 1, 1 } },
+];
+
+/*
+   glVertexAttribPointer(index, size, type, normalized, stride, *offset)
+   	boundVAO.attributes[index] = new Attribute(
+		buffer_pointer   => boundVBO + offset,
+		items_per_vertex => size,
+		items_type       => type,
+		convert_ints_to_float_range => normalized, // [-1,1] for int, [0,1] for uint
+		bytes_between_vertexes      => stride,
+	)
+
+   auto shader = new Shader(name, source)
+   shader.get_attrib_location(name)
+   	shader.get_index_for_named_shader_attr(name)
+
+   shader.get_uniform_
+*/
+
+float[] computeCircleVertexes (int n) {
+	auto vertexes = new float[n*2];
+	for (int i = 0; i < n*2; i += 2) {
+		float phi = i * PI / n;
+		vertexes[i+0] = cos(phi);
+		vertexes[i+1] = sin(phi);
+	}
+	return vertexes;
+}
+
+struct Lens {
+	float x, y, scale_x, scale_y;
+}
+
+class Shape {
+	VAO vao;
+	Buffer vbo;
+	Shader program;
+	int numVertices;
+	int anim = 0;
+
+	this(const string name, const string program_source, float[] vertices) {
+		vbo = new Buffer(vertices);
+		vao = new VAO();
+		program = new Shader(name, program_source);
+		numVertices = to!int(vertices.length);
+		// writeln(vertices);
+
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		glEnable(GL_LINE_SMOOTH);
+		glEnable(GL_MULTISAMPLE);
+		glLineWidth(2);
+	}
+
+	void setLens(Lens lens) {
+		program.bind();
+		program.uniform2f("lens_offset", lens.x, lens.y);
+		program.uniform2f("lens_scales", lens.scale_x, lens.scale_y);
+	}
+
+	void render(Circle c) {
+		anim++;
+		vao.bind();
+		vbo.bind();
+		program.bind();
+
+		float[3] color = [c.color.r, c.color.g, c.color.b];
+		float t = to!float(anim)/30;
+		program.uniform2f( "offset", c.x + sin(t)/4, c.y + cos(t)/4);
+		program.uniform1f( "radius", c.radius);
+		program.uniform3fv("color",  color);
+		GLint attrPosition = program.get_attrib_location("position");
+
+		glEnableVertexAttribArray(attrPosition);
+		glVertexAttribPointer(attrPosition, 2, GL_FLOAT, GL_FALSE, 0, null);
+		glDrawArrays(GL_LINE_LOOP, 0, numVertices/2);
+		glDisableVertexAttribArray(attrPosition);
+
+		vao.unbind();
+		vbo.unbind();
+		program.unbind();
+	}
+
+	~this() {
+		program.remove();
+		vbo.remove();
+		vao.remove();
+	}
+}
+
+class Renderer {
+	Shape circleShape;
+
+	this() {
+		circleShape = new Shape("circle", `
+			#version 120
+			vertex:
+			attribute vec2 position;
+			uniform vec2   offset;
+			uniform float  radius;
+			uniform vec2   lens_offset;
+			uniform vec2   lens_scales;
+			void main(void) {
+				gl_Position = vec4(
+					(position * radius + offset - lens_offset) / lens_scales,
+					0, 1);
+			}
+			fragment:
+			uniform vec3 color;
+			void main(void) {
+				gl_FragColor = vec4(color, 1.0);
+			}
+		`, computeCircleVertexes(40));
+	}
+	~this() {
+		delete circleShape;
+	}
+
+	void clear() {
+		glClearColor(0, 0, 0, 1);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	}
+
+	void render() {
+		clear();
+		Lens lens = {
+			0.1, 0, 32, 68
+		};
+		circleShape.setLens(lens);
+		foreach (c; circles) {
+			circleShape.render(c);
+		}
+	}
+}
